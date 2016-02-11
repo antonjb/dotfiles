fe4f4ebc802ca61c428ec0778ca0727ff740d2fa
Anton Ball <antonball@gmail.com>
2016-02-11 10:23:48 +0800

    Tweaked default software

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,2 @@
 # homebrew!
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Set symlink for Sublime Text 2
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
